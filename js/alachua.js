70b3ae15ea7c8978360a47be7846523eda808e01
gvelasquezneira <gvelasquezneira@ufl.edu>
2024-12-02 11:11:51 -0500

    commit4

--- js/alachua.js
+++ js/alachua.js
@@ -1,20 +1,3 @@
-let countyData = {};
-
-fetch('js/json.json')
-    .then(response => response.json())
-    .then(data => {
-        countyData = data;
-    })
-    .catch(error => console.error('Error loading JSON:', error));
-
-const dropdownMenu = document.querySelector('.dropdown-menu');
-dropdownMenu.addEventListener('click', (e) => {
-    const selectedCounty = e.target.textContent.trim();
-    if (countyData[selectedCounty.toLowerCase()]) {
-        loadCounty(selectedCounty.toLowerCase());
-    }
-});
-
 function loadCounty(countyId) {
     const county = countyData[countyId];
     if (!county) return;
@@ -24,13 +7,22 @@ function loadCounty(countyId) {
     let websiteList = '';
 
     county.websites.forEach(site => {
+        let buttons = '';
+        if (Array.isArray(site.urls)) {
+            site.urls.forEach((url, index) => {
+                buttons += `<a href="${url}" class="btn btn-primary mb-2 me-2" target="_blank">Link ${index + 1}</a>`;
+            });
+        } else if (site.url) {
+            buttons = `<a href="${site.url}" class="btn btn-primary mb-2 me-2" target="_blank">Visit Website</a>`;
+        }
+
         websiteList += `
-            <div class="col-md-6 mb-4">
+            <div class="website col-md-6 mb-4">
                 <div class="card h-100">
                     <div class="card-body">
                         <h5 class="card-title">${site.name}</h5>
                         <p class="card-text">${site.description}</p>
-                        <a href="${site.url}" class="btn btn-primary" target="_blank">Visit Website</a>
+                        ${buttons}
                     </div>
                 </div>
             </div>
