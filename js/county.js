7d8245dadaa6df44cc2d042086849e4ac9ec1d20
gvelasquezneira <gvelasquezneira@ufl.edu>
2024-12-02 14:30:32 -0500

    commit22

--- js/county.js
+++ js/county.js
@@ -4,18 +4,7 @@ fetch('js/county.json')
     .then(response => response.json())
     .then(data => {
         countyData = data;
-        populateCountyDropdown();
     });
-
-function populateCountyDropdown() {
-    const dropdown = document.getElementById('countyDropdown');
-    
-    for (const county in countyData) {
-        const li = document.createElement('li');
-        li.innerHTML = `<a class='dropdown-item' href='#' onclick='loadCounty("${county}")'>${countyData[county].name}</a>`;
-        dropdown.appendChild(li);
-    }
-}
 
 function loadCounty(countyId) {
     const county = countyData[countyId];
